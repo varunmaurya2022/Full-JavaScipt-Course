8bb5eac5a6bf7e0b59e63f11ccd51c269d402118
varun maurya <varunmaurya@2022>
2023-11-02 20:54:26 +0530

    modifie in shortest.js

--- Shortest_pro.js
+++ Shortest_pro.js
@@ -2,7 +2,7 @@ var a = 10;
 function b() {
    var x = 5;
 }
-console.log(window.a);
+console.log(window.a); //Window is a global object.
 console.log(a);
 //console.log(x) error:-x is not defined.
-console.log(this.a);
+console.log(this.a); //this is a global object.
