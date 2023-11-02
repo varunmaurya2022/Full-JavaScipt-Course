99d96387f257e0083a31b35523760cab5565953d
varun maurya <varunmaurya@2022>
2023-11-02 20:36:44 +0530

    add Function.js file

---
+++ Function.js
@@ -0,0 +1,14 @@
+console.log("Hello Function");
+var x = 1;
+a();
+b();
+console.log(x);
+function a() {
+   var x = 10;
+   console.log(x);
+}
+function b() {
+   var x = 100;
+   console.log(x);
+
+}
